7e09edf8559893fa4071207e098cf37e6f19b67c
Kilian Leon Kleemann <85544476+Kiliankleemann@users.noreply.github.com>
2024-03-11 15:16:57 +0100

    Create installing_squire.sh

---
+++ installing_squire.sh
@@ -0,0 +1,17 @@
+mamba create -n squire r-base=3.4.1 python=2.7 
+
+mamba install r-ggrepel r-ggplot2 r-reshape2 r-stringr r-rjson
+
+
+
+mamba install bioconductor-deseq2
+
+
+mamba install squire
+
+
+stringtie=1.3.3 samtools=1.1 bedtools=2.25.0 STAR=2.5.3a
+
+conda create -n squire
+conda activate squire
+conda install r-base=3.4.1 python=2.7
