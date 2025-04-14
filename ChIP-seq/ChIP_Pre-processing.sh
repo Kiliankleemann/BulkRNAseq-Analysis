9cc3f63c48f83e1257b715f0d7e404224a24e574
Kilian Leon Kleemann <85544476+Kiliankleemann@users.noreply.github.com>
2025-04-14 12:23:44 +0200

    update

--- ChIP-seq/ChIP_Pre-processing.sh
+++ ChIP-seq/ChIP_Pre-processing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Detected following samples'
-find ./fastq_files -name "*.fastq" -maxdepth 1 -type f -exec basename "{}" \; |  cut -d'_' -f1 | sort -u > sample_list.txt
+find ./fastq_files -name "*.fq.gz" -maxdepth 1 -type f -exec basename "{}" \; |  cut -d'_' -f1 | sort -u > sample_list.txt
 cat sample_list.txt
 
 #echo 'Unzipping files'
